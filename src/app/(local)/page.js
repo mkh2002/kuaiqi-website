5ca45e1c19916471fc2f8735d6fee8759130acc8
mkh <a2295657647@gmail.com>
2024-12-24 11:52:23 +0800

    feat:初始化配置

---
+++ src/app/(local)/page.js
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div className="container">
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/navbar/index.js
@@ -0,0 +1,6 @@
+import "./index.sass";
+import React from "react";
+
+export default function Navbar() {
+  return <nav></nav>;
+}
